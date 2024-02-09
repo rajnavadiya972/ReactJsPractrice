8624dbe343353ba2b61d3a2e29b8e8d31fdb78f2
rajnavadiya972 <rajnavadiya333@gmail.com>
2024-02-09 15:26:18 +0530

    eslint addes

--- src/component/ApiAxios.jsx
+++ src/component/ApiAxios.jsx
@@ -3,65 +3,65 @@ import axios from "axios";
 import Card from "./Card";
 
 function ApiAxios() {
-  const [post, setPost] = useState();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [loadOrError, setLoadOrError] = useState("Loading.....");
+    const [post, setPost] = useState();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [loadOrError, setLoadOrError] = useState("Loading.....");
 
-  const client = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com/posts",
-  });
+    const client = axios.create({
+        baseURL: "https://jsonplaceholder.typicode.com/posts",
+    });
 
-  //=============== Using axios without async await ===============
+    //=============== Using axios without async await ===============
 
-  //   useEffect(() => {
-  //     try {
-  //       client
-  //         .get()
-  //         .then((res) => {
-  //           console.log(res.data);
-  //           setPost(res.data);
-  //           setIsLoaded(true);
-  //         })
-  //         .catch((err) => {
-  //           setLoadOrError("Error");
-  //         });
-  //     } catch (error) {
-  //       setLoadOrError("Error");
-  //     }
-  //   }, []);
+    //   useEffect(() => {
+    //     try {
+    //       client
+    //         .get()
+    //         .then((res) => {
+    //           console.log(res.data);
+    //           setPost(res.data);
+    //           setIsLoaded(true);
+    //         })
+    //         .catch((err) => {
+    //           setLoadOrError("Error");
+    //         });
+    //     } catch (error) {
+    //       setLoadOrError("Error");
+    //     }
+    //   }, []);
 
-  //=============== Using axios with async await ===============
+    //=============== Using axios with async await ===============
 
-  useEffect(() => {
-    try {
-      const fetchData = async () => {
-        const data = await client.get();
-        setPost(data.data);
-        setIsLoaded(true);
-      };
-      fetchData();
-    } catch (error) {
-      setLoadOrError("Error");
-    }
-  }, []);
+    useEffect(() => {
+        try {
+            const fetchData = async () => {
+                const data = await client.get();
+                setPost(data.data);
+                setIsLoaded(true);
+            };
+            fetchData();
+        } catch (error) {
+            setLoadOrError("Error");
+        }
+    }, []);
 
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(4, 1fr)",
-        gridGap: 20,
-      }}
-    >
-      {isLoaded ? (
-        post.map((p) => {
-          return <Card key={p.id} data={p} />;
-        })
-      ) : (
-        <h1>{loadOrError}</h1>
-      )}
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(4, 1fr)",
+                gridGap: 20,
+            }}
+        >
+            {isLoaded ? (
+                post.map((p) => {
+                    return <Card key={p.id} data={p} />;
+                })
+            ) : (
+                <h1>{loadOrError}</h1>
+            )}
+        </div>
+    );
 }
 
 export default ApiAxios;
--- src/component/ApiFetch.jsx
+++ src/component/ApiFetch.jsx
@@ -2,70 +2,70 @@ import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
 function ApiFetch() {
-  const [post, setPost] = useState();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [loadOrError, setLoadOrError] = useState("Loading.....");
+    const [post, setPost] = useState();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [loadOrError, setLoadOrError] = useState("Loading.....");
 
-  //=============== Using Fetch with async await ============
+    //=============== Using Fetch with async await ============
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       setLoadOrError("Loading.....");
-  //       const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-  //       const posts = await data.json();
-  //       setPost(posts);
-  //       setIsLoaded(true);
-  //     } catch (err) {
-  //       setLoadOrError("Error");
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       setLoadOrError("Loading.....");
+    //       const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //       const posts = await data.json();
+    //       setPost(posts);
+    //       setIsLoaded(true);
+    //     } catch (err) {
+    //       setLoadOrError("Error");
+    //     }
+    //   };
+    //   fetchData();
+    // }, []);
 
-  //=============== Using Fetch without async await ===============
+    //=============== Using Fetch without async await ===============
 
-  useEffect(() => {
-    const fetchData = () => {
-      setLoadOrError("Loading.....");
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => {
-          return res.json();
-        })
-        .then(
-          (data) => {
-            const posts = data;
-            setPost(posts);
-            setIsLoaded(true);
-          },
-          (err) => {
-            setLoadOrError("Error1", err);
-          },
-        )
-        .catch((err) => {
-          setLoadOrError("Error2", err);
-        });
-    };
-    fetchData();
-  }, []);
+    useEffect(() => {
+        const fetchData = () => {
+            setLoadOrError("Loading.....");
+            fetch("https://jsonplaceholder.typicode.com/posts")
+                .then((res) => {
+                    return res.json();
+                })
+                .then(
+                    (data) => {
+                        const posts = data;
+                        setPost(posts);
+                        setIsLoaded(true);
+                    },
+                    (err) => {
+                        setLoadOrError("Error1", err);
+                    },
+                )
+                .catch((err) => {
+                    setLoadOrError("Error2", err);
+                });
+        };
+        fetchData();
+    }, []);
 
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(4, 1fr)",
-        gridGap: 20,
-      }}
-    >
-      {isLoaded ? (
-        post.map((p) => {
-          return <Card key={p.id} data={p} />;
-        })
-      ) : (
-        <h1>{loadOrError}</h1>
-      )}
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(4, 1fr)",
+                gridGap: 20,
+            }}
+        >
+            {isLoaded ? (
+                post.map((p) => {
+                    return <Card key={p.id} data={p} />;
+                })
+            ) : (
+                <h1>{loadOrError}</h1>
+            )}
+        </div>
+    );
 }
 
 export default ApiFetch;
